291ce7cf6372218125cb85bbd54c56572f90c751
Shriram Natarajan <shnatara@Shriram-mbp.local>
2016-12-05 00:57:43 -0800

    fixed stringconcat issues

--- AzurePolicyCreate.sh
+++ AzurePolicyCreate.sh
@@ -1,10 +1,10 @@
 python AzurePolicyCreate.py
 #azure login -e AzureCloud
 # RESOURCEGROUPID=$(azure group list --json | jq '.[] | select (.name | contains ("shnatarapolicy"))' | jq '.id')
-RESOURCEGROUPID=""+$(azure group create shnatarapolicy09 westus --json | jq .id)
+RESOURCEGROUPID=$(azure group create shnatarapolicy10 westus --json | jq .id)
 echo $RESOURCEGROUPID
 
-POLICYID=""$(azure policy definition create --name regionPolicyDefinition1 --description "Policy to allow resource creation only in certain regions" --policy AzureStackPolicy.json --json | jq '.id')
+POLICYID=$(azure policy definition create --name regionPolicyDefinition1 --description "Policy to allow resource creation only in certain regions" --policy AzureStackPolicy.json --json | jq '.id')
 echo $POLICYID
 
 #azure policy assignment delete --name hellpolicy --scope "/subscriptions/d34bd6cc-8d7e-451b-ace3-cd05c69f82d0/resourceGroups/shnatarapolicy01"
