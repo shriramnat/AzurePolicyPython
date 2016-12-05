64e795714f869babedf2cd9303ad3f20a2eac56f
Shriram Natarajan <shnatara@shriram-mbp.guest.corp.microsoft.com>
2016-12-05 15:28:59 -0800

    Updates to text

--- AzurePolicyCreate.sh
+++ AzurePolicyCreate.sh
@@ -3,17 +3,17 @@ POLICY=$(python AzurePolicyCreate.py)
 # Login to Azure. Comment this line out if already logged in
 azure login -e AzureCloud
 
-RESOURCEGROUPID=$(azure group create shnatarapolicy31 westus --json | jq -r .id)
+RESOURCEGROUPID=$(azure group create shnatarapolicy32 westus --json | jq -r .id)
 echo "Created Resource Group with ID " $RESOURCEGROUPID
 
-POLICYID=$(azure policy definition create --name regionPolicyDefinition1 --description "Policy to allow resource creation only in certain regions" --policy-string "$POLICY" --json | jq -r '.id')
+POLICYID=$(azure policy definition create --name AzureStackPolicy --description "Policy to allow only Azure Stack compatible resources to be created" --policy-string "$POLICY" --json | jq -r '.id')
 echo "Created Policy Definition with ID " $POLICYID
 
 # To create policy from a file instead of a string
-# POLICYID=$(azure policy definition create --name regionPolicyDefinition1 --description "Policy to allow resource creation only in certain regions" --policy AzureStackPolicy.json --json | jq -r '.id')
+# POLICYID=$(azure policy definition create --name AzureStackPolicy --description "Policy to allow only Azure Stack compatible resources to be created" --policy AzureStackPolicy.json --json | jq -r '.id')
 
 echo "Creating Policy Assignment on the Resource Group..."
-azure policy assignment create --name hellopolicy --policy-definition-id $POLICYID --scope $RESOURCEGROUPID --verbose
+azure policy assignment create --name AzureStackPolicy --policy-definition-id $POLICYID --scope $RESOURCEGROUPID --verbose
 
 # MISC helper cmds
 # RESOURCEGROUPID=$(azure group list --json | jq '.[] | select (.name | contains ("shnatarapolicy"))' | jq '.id')
